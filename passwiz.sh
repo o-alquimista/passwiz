c9be699bcf3f390fb75752c5f41d11f13d883939
Douglas Silva <doug.hs@protonmail.ch>
2020-11-11 22:15:52 -0300

    Update copyright year

--- passwiz.sh
+++ passwiz.sh
@@ -3,7 +3,7 @@
 #
 # PassWiz, a random password generator.
 #
-# Copyright 2019 Douglas Silva (0x9fd287d56ec107ac)
+# Copyright 2019-2020 Douglas Silva (0x9fd287d56ec107ac)
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
