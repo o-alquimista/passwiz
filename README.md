d86ab7798451e35a822f23c5cde6cc00b7f548aa
Douglas Silva <doug.hs@protonmail.ch>
2023-07-25 14:14:09 -0300

    Allow specifying arbitrary characters; added --version option

--- README.md
+++ README.md
@@ -13,25 +13,8 @@ $ chmod +x ~/bin/passwiz
 $ passwiz --help
 ```
 
-```
-Usage: passwiz [-h, --help] [-s SIZE, --size SIZE] [-c CONSTRAINT, --constraint CONSTRAINT]
-
-PassWiz, a random string generator.
-
-Options:
-  -c, --constraint   Determines what type of characters can be used to make the string
-  -s, --size         Select the size of the generated string (default: 20)
-  -h, --help         Print this help, then exit
-
-Character type constraints (--constraint argument):
-  [1] Numeric
-  [2] Alphabetical
-  [3] Alphanumerical
-  [4] Alphanumerical and punctuation (default)
-```
-
 ## License
-Copyright 2019-2021 Douglas Silva (0x9fd287d56ec107ac)
+Copyright 2019-2023 Douglas Silva (0x9fd287d56ec107ac)
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
--- passwiz.sh
+++ passwiz.sh
@@ -3,7 +3,7 @@
 #
 # PassWiz, a random string generator.
 #
-# Copyright 2019-2021 Douglas Silva (0x9fd287d56ec107ac)
+# Copyright 2019-2023 Douglas Silva (0x9fd287d56ec107ac)
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -19,12 +19,18 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #
 
-opts=`getopt -n 'passwiz' -o c:s:h --long constraint:,size:,help -- "$@"`
+opts=$(getopt \
+  --name 'passwiz' \
+  --options c:s:hv \
+  --longoptions constraint:,size:,help,version \
+  -- "$@")
 
-if [[ $? != 0 ]]; then
+if [[ $? -ne 0 ]]; then
     echo "Failed parsing options. See --help." >&2;
     exit 1;
 fi
+
+readonly VERSION_TAG="v2.1.0"
 
 # Constraint options
 readonly CONSTRAINT_DIGIT=1
@@ -34,6 +40,7 @@ readonly CONSTRAINT_ALNUM_PUNCT=4
 
 # Set default settings
 help=false
+version=false
 size=20
 constraint=$CONSTRAINT_ALNUM_PUNCT
 
@@ -45,13 +52,14 @@ while (( $# )); do
     -c | --constraint ) constraint="$2"; shift 2 ;;
     -s | --size ) size="$2"; shift 2 ;;
     -h | --help ) help=true; shift ;;
+    -v | --version ) version=true; shift ;;
     -- ) shift; break ;;
     * ) break ;;
   esac
 done
 
 usage=$(cat << EOF
-Usage: passwiz [-h, --help] [-s SIZE, --size SIZE] [-c CONSTRAINT, --constraint CONSTRAINT]
+Usage: passwiz [-v, --version] [-h, --help] [-s SIZE, --size SIZE] [-c CONSTRAINT, --constraint CONSTRAINT]
 
 PassWiz, a random string generator.
 
@@ -59,12 +67,14 @@ Options:
   -c, --constraint   Determines what type of characters can be used to make the string
   -s, --size         Select the size of the generated string (default: 20)
   -h, --help         Print this help, then exit
+  -h, --version      Print the version tag, then exit
 
 Character type constraints (--constraint argument):
   [1] Numeric
   [2] Alphabetical
   [3] Alphanumerical
   [4] Alphanumerical and punctuation (default)
+  [5] Other - specify which characters are allowed; see "man tr" for format)
 EOF
 )
 
@@ -73,17 +83,23 @@ if [[ $help == true ]]; then
     exit
 fi
 
+if [[ $version == true ]]; then
+    echo $VERSION_TAG
+    exit
+fi
+
 if ! [[ $size =~ ^[0-9]+$ ]]; then
     echo "Invalid size. See --help." >&2
     exit 1
 fi
 
+# see: man tr
 case $constraint in
     $CONSTRAINT_DIGIT ) constraint="[:digit:]";;
     $CONSTRAINT_ALPHA ) constraint="[:alpha:]";;
     $CONSTRAINT_ALNUM ) constraint="[:alnum:]";;
     $CONSTRAINT_ALNUM_PUNCT ) constraint="[:alnum:][:punct:]";;
-    * ) echo "Invalid constraint. See --help." >&2; exit 1 ;;
+    * ) constraint="$constraint";;
 esac
 
 # This is where the string is retrieved, using the same method
@@ -91,4 +107,3 @@ esac
 read -r -n $size string < <(LC_ALL=C tr -dc "$constraint" < /dev/urandom)
 
 echo "$string"
-
